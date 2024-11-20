a21f3df33a6b3e0034d6e3a4c41e145b6c11220f
Yashraj0888 <bloodymoonknite888@gmail.com>
2024-11-20 21:03:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className='flex justify-center items-center h-screen'>
+        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+        <p>this is a paragraph</p>
+      </div>
+    </>
+  )
+}
+
+export default App
